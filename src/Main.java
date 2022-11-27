3d68939c5e948f3103bf24114f65fb269a397dde
Garik-create <pfdfkbybujhm@yandex.ru>
2022-11-27 12:29:49 +0300

    Начальный комит.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+//        String[] strings = {"first", "first", "second", "third", "forth",
+//                "fifth", "second", "third", "forth", "fifth"};
+//        System.out.println(Arrays.toString(strings));
+//        String[] newStrings = {};
+//        List<String> newList = new ArrayList<>();
+//        for (String s : strings) {
+//            if (!newList.contains(s)) {
+//                newList.add(s);
+//            }
+//        }
+//        System.out.println(Arrays.toString(newList.toArray()));
 
     }
 }
