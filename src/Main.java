6fa42a1d9069856435adf14c20db2749642d0b59
Garik-create <pfdfkbybujhm@yandex.ru>
2022-11-27 13:39:01 +0300

    Выполнил вторую задачу.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String[] strings = {"first", "first", "second", "third", "forth",
+  /*      String[] strings = {"first", "first", "second", "third", "forth",
                 "fifth", "second", "third", "forth", "fifth"};
         System.out.println(Arrays.toString(strings));
         String[] newStrings = {};
@@ -15,6 +15,23 @@ public class Main {
             }
         }
         System.out.println(Arrays.toString(newList.toArray()));
+*/
 
+//        task 2
+        List<String> names = new ArrayList<>();
+        names.add("Ivan");
+        names.add("Peter");
+        names.add("Vasily");
+        names.add("Dmitry");
+        names.add("Semen");
+        System.out.println(names);
+        List<String> newNames = new ArrayList<>();
+
+        for (String name : names) {
+            String substring = name.substring(1);
+            newNames.add(substring);
+        }
+
+        System.out.println(newNames);
     }
 }
